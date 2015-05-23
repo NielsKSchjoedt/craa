5c8828440fb641c49a6323b9f34a5d5abc102c39
NielsKSchjoedt <nk.schjoedt@gmail.com>
2015-05-23 05:01:01 +0200

    Add cloud front

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,8 @@ Rails.application.configure do
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
+    :url => ':s3_alias_url',
+    :s3_host_alias => 'craa-assets.s3.amazonaws.com'
   }
 
   # Prepend all log lines with the following tags.
